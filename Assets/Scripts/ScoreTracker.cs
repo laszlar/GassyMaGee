cbabcd827ae82bb17a75052e1e5da91bbeba3ba2
laszlar <laszlar@gmail.com>
2016-03-23 21:59:47 -0500

    Revert "• Added death animation concept"
    This reverts commit 13f7cc020fe8964abccc53f8af6724c030e8a680.

--- Assets/Scripts/ScoreTracker.cs
+++ Assets/Scripts/ScoreTracker.cs
@@ -7,12 +7,13 @@ public class ScoreTracker : MonoBehaviour {
 	public int pts = 0;
 
 	void Start() {
-		pts = GameObject.Find("Player").GetComponent<PlayerMovement>().points;
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     { 
+		pts = GameObject.Find("Player").GetComponent<PlayerMovement>().points;
 		Text text = gameObject.GetComponent<Text> ();
 		text.text = " " + pts;
 	}

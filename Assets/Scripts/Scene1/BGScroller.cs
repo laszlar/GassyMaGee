f0b55feebf2c7a3688e7316bc6b6514e8491307b
Laszlo Kecskes <laszlar@gmail.com>
2016-04-22 09:49:32 -0500

    Adjust the parachute power up - it's working now perfectly!

--- Assets/Scripts/Scene1/BGScroller.cs
+++ Assets/Scripts/Scene1/BGScroller.cs
@@ -7,11 +7,12 @@ public class BGScroller : MonoBehaviour
     public float scrollSpeed;
     public float tileSize;
     float halfScrollSpeed;
+    float newPosition = 0f;
 
     PlayerMovement script;
 
     Vector2 startPosition;
-    Vector2 newPosition;
+    //Vector2 newPosition;
 
     // Use this for initialization
     void Start()
@@ -23,7 +24,7 @@ public class BGScroller : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (!script.scrollerThing) //If power up NOT activated scroll at regular speed.
+       /* if (!script.scrollerThing) //If power up NOT activated scroll at regular speed.
         {
             float floatNewPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSize);
             newPosition = new Vector2(-floatNewPosition, 0f);
@@ -34,8 +35,22 @@ public class BGScroller : MonoBehaviour
             halfScrollSpeed = scrollSpeed / 2;
             float floatNewPosition = Mathf.Repeat(Time.time * halfScrollSpeed, tileSize);
             newPosition = new Vector2(-floatNewPosition, 0f);
-            transform.position = startPosition + newPosition;
+            transform.position = startPosition + newPosition;*/
+
+      if(!script.scrollerThing)
+        {
+            newPosition += Time.deltaTime * scrollSpeed;
+        }  
+      else
+        {
+            halfScrollSpeed = scrollSpeed / 2;
+            newPosition += Time.deltaTime * halfScrollSpeed;
         }
+
+        while (newPosition >= tileSize)
+            newPosition -= tileSize;
+        transform.position = startPosition + (Vector2.left * newPosition);
     }
 }
+
 

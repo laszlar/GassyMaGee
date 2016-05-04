850ca2e8d84d992e42e67eb6a877e0f49e440392
Laszlo Kecskes <laszlar@gmail.com>
2016-05-04 13:42:45 -0500

    Tried implementing timer & emission

--- Assets/Scripts/Scene1/FartEffect.cs
+++ Assets/Scripts/Scene1/FartEffect.cs
@@ -3,31 +3,40 @@ using System.Collections;
 
 public class FartEffect : MonoBehaviour
 {
-    public ParticleSystem fart;
+    public ParticleSystem.EmissionModule fart;
+    public float timer;
 
     void Start()
     {
-       /* ParticleSystem fart = GetComponent<ParticleSystem>();
-        ParticleSystem.EmissionModule isFarting = fart.emission;
-        isFarting.enabled = false;
-        //farting = GetComponent<ParticleSystem>().emission.enabled = false;
-        //farting.enabled = false;*/
+        fart.enabled = false;
     }
-	
 
 	void Update ()
     {
-	    if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
-        {
-            ParticleSystem fart = GetComponent<ParticleSystem>();
-            ParticleSystem.EmissionModule isFarting = fart.emission;
-            isFarting.enabled = true;
-        }
-        else
-        {
-            ParticleSystem fart = GetComponent<ParticleSystem>();
-            ParticleSystem.EmissionModule isFarting = fart.emission;
-            isFarting.enabled = false;
-        }
+        CheckInput();
+        Farted();
+        SubtractTimer();
 	}
+
+    void CheckInput()
+    {
+        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            //we shall see if this works.
+            timer += 0.25f;
+        }
+    }
+
+    void Farted()
+    {
+        //activate that fart if more than 0 seconds.
+        fart.enabled = (timer > 0) ? true : false;
+    }
+
+    void SubtractTimer()
+    {
+        //Subtract one second from the timer, and set it to zero if less than one second
+        timer -= Time.deltaTime;
+        timer = (timer <= 0) ? 0 : timer;
+    }
 }

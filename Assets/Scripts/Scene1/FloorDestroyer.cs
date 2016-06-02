8275d4b5d69e2622d43773f84f23406d7cee8c04
laszlar <laszlar@gmail.com>
2016-06-01 23:18:53 -0500

    Got the planks to be destroyed.

---
+++ Assets/Scripts/Scene1/FloorDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorDestroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+
+            Destroy(coll.gameObject);
+            Debug.Log("I hit the plank");
+       
+    }
+}

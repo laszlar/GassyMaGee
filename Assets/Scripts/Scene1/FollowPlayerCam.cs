0ce5fe9a3c9a859d5c26cca621abab7dd5fda408
laszlar <laszlar@gmail.com>
2016-06-21 17:36:35 -0500

    Added a lerp to the camera movement (smooth follow player cam)

---
+++ Assets/Scripts/Scene1/FollowPlayerCam.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayerCam : MonoBehaviour
+{
+
+    public float interVelocity;
+    public float minDistance;
+    public float followDistance;
+    public GameObject player;
+    public Vector3 offset;
+    Vector3 targetPos;
+
+	// Use this for initialization
+	void Start ()
+    {
+        targetPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        //transform.position = player.transform.position + offset;
+        Vector3 posZ = transform.position; //camera's position
+        posZ.z = player.transform.position.z; //make sure camaera's z position and player's is the same
+
+        Vector3 targetDirection = (player.transform.position - posZ); //this tells the camera in which direction the player is moving/which direction to move
+
+        interVelocity = targetDirection.magnitude * 5f; //The actual value on how quick to move the camera? 
+
+        targetPos = transform.position + (targetDirection.normalized * interVelocity * Time.deltaTime); //the actual moving of the camera
+
+        transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+
+
+	}
+}

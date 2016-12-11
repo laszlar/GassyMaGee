9f6289e25793e96c6e41bc400230e59725de0c63
laszlar <laszlar@gmail.com>
2016-12-11 16:12:00 -0600

    Putting in the Posts and Rope to spawn in...

---
+++ Assets/Scripts/Scene1/MovePostsRopeBG.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePostsRopeBG : MonoBehaviour
+{
+    public GameObject postsAndFuckingRope;
+    public GameObject player;
+    float currentPosition;                              
+    float xValueOfPostsAndRope = 3.293226313f;
+    float newPosition;
+    bool spawnAlright;
+    Vector2 xPosition;
+    float playerPosition;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerPosition = player.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (playerPosition < 3)
+            spawnAlright = true;
+        
+        if (spawnAlright)
+        {
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        currentPosition = transform.position.x;                                 //get the  current position
+        newPosition = currentPosition + xValueOfPostsAndRope;
+        xPosition = new Vector2(newPosition, 0);                                //save the Vector2
+        Instantiate(postsAndFuckingRope, xPosition, transform.rotation);        //spawn bitches.
+    }
+}

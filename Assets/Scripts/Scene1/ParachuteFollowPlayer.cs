79ba2882fe166a0503803c8cd1fcd5690ad2b517
laszlar <laszlar@gmail.com>
2017-01-04 21:43:16 -0600

    Added the Parachute animation, and put it in as a prefab to call while powerup (didn't finish that)

---
+++ Assets/Scripts/Scene1/ParachuteFollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParachuteFollowPlayer : MonoBehaviour
+{
+    private GameObject player;
+    private float offsetX = 0.30f;
+    private float offsetY = 0.05f;
+    private Vector2 position;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.Find("Player");	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = position;
+        position = new Vector2((player.transform.position.x - offsetX), (player.transform.position.y + offsetY));
+	}
+}

ffa34c4d5e84dec2bb96bb98f1e4b240c76fcbb3
laszlar <laszlar@gmail.com>
2017-01-09 21:26:02 -0600

    modified Scene3 (store)

---
+++ Assets/Scripts/Scene3/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : Singleton<Inventory> {
+
+    [SerializeField]  private GameObject paint;
+    [SerializeField] private GameObject parachute;
+    [SerializeField] private PlayerMovement playerScript;
+
+    List<GameObject> InventoryList = new List<GameObject>();
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/Scene3/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    public static T Instance { get; protected set; }
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            throw new System.Exception("An Instance of this singleton already exists.");
+        }
+        else
+        {
+            Instance = (T)this;
+        }
+    }
+}

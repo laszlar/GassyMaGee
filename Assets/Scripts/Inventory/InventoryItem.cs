61ea91ea085552355fc05aa63cf38baab0d89efc
laszlar <laszlar@gmail.com>
2017-01-16 23:03:26 -0600

    Worked on Store with Tony :)

---
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem : MonoBehaviour
+{
+    public Texture2D inventoryIcon;
+    public string inventoryLabel;
+    public bool activatePaint;
+    public bool activateParachute;
+
+    [SerializeField]
+    private GameObject paint;
+    [SerializeField]
+    private GameObject parachute;
+
+    private void Start()
+    {
+        paint = GameObject.Find("paint");
+        parachute = GameObject.Find("parachute");
+    }
+
+    //activate powerups
+	public virtual void Activate ()
+    {
+        if (activatePaint)
+        {
+            paint.gameObject.SetActive(true);
+        }
+
+        if (activateParachute)
+        {
+            parachute.gameObject.SetActive(true);
+        }
+    }
+	
+    //deactivate powerups
+	public virtual void Deactivate ()
+    {
+        if (activatePaint)
+        {
+            paint.gameObject.SetActive(false);
+        }
+
+        if (activateParachute)
+        {
+            parachute.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : Singleton<Inventory>
+{
+    private bool inventoryFull;
+
+    [SerializeField]
+    private GameObject paint;
+    [SerializeField]
+    private GameObject parachute;
+    [SerializeField]
+    private PlayerMovement playerScript;
+    [SerializeField]
+
+
+    List <GameObject> inventoryItems = new List<GameObject>();
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    
+    // Use this for initialization
+    void Start ()
+    {
+        paint = GameObject.Find("PaintImage");
+        parachute = GameObject.Find("ParachuteImage");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (inventoryItems.Count == 3)
+        {
+            inventoryFull = true;
+        }
+
+
+        if (!inventoryFull)
+        {
+            inventoryItems.Add(gameObject);
+        }
+        else
+        {
+            return;
+        }
+
+        
+	}
+
+    private void ParachuteButton()
+    {
+        if 
+    }
+
+    private void PaintButton()
+    {
+
+    }
+}

93dfe4692c249f6e3b6601509483d9fb6bd5428e
laszlar <laszlar@gmail.com>
2017-02-19 13:30:53 -0600

    Particle system follows player now

---
+++ Assets/Scripts/Scene1/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    private GameObject player;
+    public Vector2 currentPos;
+    public float yPos;
+    private float playerXCoordinate;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate ()
+    {
+        transform.position = currentPos;
+        currentPos = new Vector2(player.transform.position.x, yPos);
+	}
+}

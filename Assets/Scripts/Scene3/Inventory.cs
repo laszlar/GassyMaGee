7b29a4c64f4e8b541e139a60b3dfdd8dee552816
Anthony Michelizzi <cynical89@gmail.com>
2017-02-22 20:02:49 -0800

    Added basic inventory

---
+++ Assets/Scripts/Scene3/Inventory.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory {
+
+    public List<Items> InventoryList = new List<Items>();
+    public bool IsFull;
+
+    public enum Items
+    {
+        Paint,
+        Chute
+    }
+
+    public bool IsInventoryFull()
+    {
+        if (InventoryList.Count >= 3)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    public void SaveInventory()
+    {
+        if(PlayerPrefs.HasKey("Inv0") || PlayerPrefs.HasKey("Inv1") || PlayerPrefs.HasKey("Inv2"))
+        {
+            DeleteAllInvKeys();
+        }
+
+        for (var i = 0; i < 3; i++)
+        {
+            PlayerPrefs.SetInt("Inv" + i, Convert.ToInt32(InventoryList[i]));
+        }
+
+        PlayerPrefs.Save();
+    }
+
+    public void LoadInventory()
+    {
+        if(PlayerPrefs.HasKey("Inv0"))
+        {
+            InventoryList.Add((Items)PlayerPrefs.GetInt("Inv0"));
+        }
+        if (PlayerPrefs.HasKey("Inv1"))
+        {
+            InventoryList.Add((Items)PlayerPrefs.GetInt("Inv1"));
+        }
+        if (PlayerPrefs.HasKey("Inv2"))
+        {
+            InventoryList.Add((Items)PlayerPrefs.GetInt("Inv2"));
+        }
+    }
+
+    public void DeleteAllInvKeys()
+    {
+        PlayerPrefs.DeleteKey("Inv0");
+        PlayerPrefs.DeleteKey("Inv1");
+        PlayerPrefs.DeleteKey("Inv2");
+    }
+}
--- Assets/Scripts/Scene3/Store.cs
+++ Assets/Scripts/Scene3/Store.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Store : MonoBehaviour {
 
+    private readonly Inventory _inv = new Inventory();
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,31 @@ public class Store : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        _inv.IsFull = _inv.IsInventoryFull();   
 	}
+
+    public void PaintButton()
+    {
+        if (!!_inv.IsFull)
+            return;
+
+        if (!_inv.IsFull)
+        {
+            Debug.Log("Paint added to inventory");
+            _inv.InventoryList.Add(Inventory.Items.Paint);
+        }
+    }
+
+    public void ChuteButton()
+    {
+        if (!!_inv.IsFull)
+            return;
+
+        if (!_inv.IsFull)
+        {
+            Debug.Log("Parachute added to inventory");
+            _inv.InventoryList.Add(Inventory.Items.Chute);
+        }
+    }
+
 }

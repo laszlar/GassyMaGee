2695a38a4f825736c7474225e9807b2b84038e7c
laszlar <laszlar@gmail.com>
2017-03-07 05:54:58 -0600

    Created a wave placeholder that moves the waves appropriately.

---
+++ Assets/Scripts/Scene1/WaveHolderBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveHolderBehaviour : MonoBehaviour
+{
+    float movementY;
+    float movementX;
+    float movement;
+    float timeIndex;
+    float omegaY = 300;
+    float randomizeYMovement;
+
+	// Use this for initialization
+	void Start ()
+    {
+        randomizeYMovement = Random.Range(0.005f, 0.01f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {      
+        //increasing the number dividing the time doesn't just slow it down, but it also increases the up and down movement
+        timeIndex += Time.deltaTime / 20;
+        //This makes it do the up and down movement
+        movementY = Mathf.Sin(omegaY * timeIndex);
+        //this line makes it actually move
+        transform.Translate((-0.3f * Time.deltaTime), randomizeYMovement * movementY, 0f);
+    }
+
+    float Hermite(float t)
+    {
+        return -t * t * t * 2 + t * t * 3;
+    }
+}
--- Assets/Scripts/Scene1/WaveSpin.cs
+++ Assets/Scripts/Scene1/WaveSpin.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class WaveSpin : MonoBehaviour
 {
     public float spinSpeed;
+    private float uppersAndDowners;
+    private float randomHeight;
+    Vector3 moveLeft;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
-	void LateUpdate ()
+    void FixedUpdate()
     {
+        /*randomHeight = Random.Range(0, 1);
+        uppersAndDowners = Mathf.PingPong(Time.time, 0.5f);
+
+        //moveLeft = new Vector3((-1 * Time.deltaTime), -uppersAndDowners, 0f);
+
+        //you can use this line of code with transform.rotate, must use += operator
+        //transform.position += moveLeft;
+        */
+              
+        //rotate the wave on its Z axis
         transform.Rotate(0, 0, spinSpeed * Time.deltaTime);
-	}
+    }
 }

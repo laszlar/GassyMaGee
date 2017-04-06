49bb773145871ff12fa6f85388b8ca04a3f9d3a5
HOMEBREW-PC\Laszlo <laszlar@gmail.com>
2017-04-05 21:54:22 -0500

    BG looping needs to be verified, but seems to work 100%

--- Assets/Scripts/Scene1/BGLooper.cs
+++ Assets/Scripts/Scene1/BGLooper.cs
@@ -4,14 +4,60 @@ using System.Collections;
 public class BGLooper : MonoBehaviour {
 
     public int numBGPanels = 6;
+    private int numCloudPanels = 4;
+    public float actualWidth;
+    private float bgWidth;
+    private float cloudWidth;
+    private GameObject deepOcean0;
+    private GameObject deepOcean1;
+    private GameObject deepOcean2;
+    private GameObject sand0;
+    private GameObject sand1;
+    private GameObject sand2;
+
+    void Start()
+    {
+        /*deepOcean0 = GameObject.Find("Deepocean");
+        deepOcean1 = GameObject.Find("Deepocean");
+        deepOcean2 = GameObject.Find("Deepocean");
+
+        sand0 = GameObject.Find("Sand0");
+        sand1 = GameObject.Find("Sand1");
+        
+        sand2 = GameObject.Find("Sand2");
+    */
+    }
 
 	void OnTriggerEnter2D (Collider2D collider)
     {
         float widthOfBGObject = ((BoxCollider2D)collider).size.x;
 
-        Vector3 pos = collider.transform.position;
-        pos.x += widthOfBGObject * numBGPanels;
-        collider.transform.position = pos;
+        if (collider.name == "Deepocean" || collider.name == "Deepocean1" || collider.name == "Deepocean2")
+        {
+            Vector2 actualPos = collider.transform.position;
+            actualWidth = widthOfBGObject * 0.3f;
+            actualPos.x += actualWidth * (numBGPanels / 2);
+            collider.transform.position = actualPos;
+        }
+
+        if (collider.name == "subprototype_edificios01_0" || collider.name == "subprototype_dificios01.1_0" || 
+            collider.name == "subprototype_edificios02_0" || collider.name == "subprototype_edificios02.1_0" || 
+            collider.name == "subprototype_edificios06_0" || collider.name == "subprototype_edificios06.1_0")
+        {
+            Vector2 bgPos = collider.transform.position;
+            bgWidth = widthOfBGObject * 0.25f;
+            bgPos.x += bgWidth * numBGPanels;
+            collider.transform.position = bgPos;
+        }
+
+        if (collider.name == "clouds_0" || collider.name == "clouds_0 (1)" ||
+            collider.name == "clouds_0 (2)" || collider.name == "clouds_0 (3)")
+        {
+            Vector2 cloudPos = collider.transform.position;
+            cloudWidth = widthOfBGObject * 0.25f;
+            cloudPos.x += cloudWidth * numCloudPanels;
+            collider.transform.position = cloudPos;
+        }
     }
 
 

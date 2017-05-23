e0ae91c57ac5c97b51e52158ff4f19d6c7fb5752
HOMEBREW-PC\Laszlo <laszlar@gmail.com>
2017-05-22 22:04:13 -0500

    if bomb is spinning, make sure BOOM effect is upright :)

--- Assets/Scripts/Scene1/BoomEffect.cs
+++ Assets/Scripts/Scene1/BoomEffect.cs
@@ -8,9 +8,12 @@ public class BoomEffect : MonoBehaviour
     public GameObject bomb;
     ParticleSystem boomEffect;
 
+    float zValue = 0f;
+
     void Start()
     {
         bombScript = transform.parent.GetComponent<Bomb>();
+        bomb = transform.parent.gameObject;
         boomEffect = GetComponent<ParticleSystem>();
         boomEffect.Pause();
     }
@@ -20,6 +23,10 @@ public class BoomEffect : MonoBehaviour
         if (bombScript.playerHitBomb)
         {
             boomEffect.Play();
+            if (transform.position.z != 0)
+            {
+                transform.position = new Vector3(bomb.transform.position.x, bomb.transform.position.y, zValue);
+            }
         }
     }
 }

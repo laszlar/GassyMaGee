68c7b34cd56528b23d09fa1d57ade4b0a736879f
HOMEBREW-PC\Laszlo <laszlar@gmail.com>
2017-05-23 20:57:18 -0500

    Check to see if score is a certain amount before launching - need to complete

---
+++ Assets/Scripts/Scene1/BombLauncher.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombLauncher : MonoBehaviour
+{
+    float timer = 0f;
+    bool launchBombs;
+    public GameObject bomb;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 3.0f)
+        {
+            timer = 0f;
+            if (ScoreTracker.score >= 15)
+            {
+                launchBombs = true;
+            }
+        }
+	}
+
+    void SpawnBombs()
+    {
+        if (!launchBombs)
+            return;
+
+        //Instantiate(bomb, );
+
+
+    }
+}

9a7e28e2ff34a49d9a043d1d5433e39f66bca745
HOMEBREW-PC\Laszlo <laszlar@gmail.com>
2017-10-28 20:54:18 -0500

    Created a random array for Game Over screen

---
+++ Assets/Scripts/Scene2/RandomStrings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomStrings : MonoBehaviour
+{
+    public Text thisText;
+    string[] randomText = new string[] {"Oh, your miserable fate!", "Your poor little head!", "Better luck next time!",
+                                         "This is how the cookie crumbles!", "Let this not be the end!", "Oh, no!"}; 
+	
+	void Start ()
+    {
+        Random.seed = System.DateTime.Now.Millisecond;
+        thisText = this.GetComponent<Text>();
+        thisText.text = randomText[(Random.Range(0, 5))];
+	}
+}

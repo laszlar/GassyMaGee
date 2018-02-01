40b15d25e8ae322c4676ccdc5daf148a6d852e5f
Anthony Michelizzi <14186276+cynical89@users.noreply.github.com>
2018-02-01 05:53:37 -0800

    Quick & dirty loading screen
    This solves a little bit of the scene loading issues. The scene load will still need some optimization.

--- Assets/Scripts/Scene0/SceneManagement.cs
+++ Assets/Scripts/Scene0/SceneManagement.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SceneManagement : MonoBehaviour {
+
+    private bool _loadScene = false;
+    private string _scene = "Scene1";
+    [SerializeField] private Text _loadingText;
 
 	// Use this for initialization
 	void Start () {
@@ -12,9 +17,33 @@ public class SceneManagement : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-	    if (Time.timeSinceLevelLoad >= 5f || Input.GetKeyDown (KeyCode.Mouse0))
+	    if (!_loadScene)
         {
-            SceneManager.LoadScene("Scene1");
+            _loadScene = true;
+            _loadingText.text = "Loading...";
+            StartCoroutine(LoadNewScene());
+        }
+
+        if (_loadScene)
+        {
+            _loadingText.color = new Color(_loadingText.color.r, _loadingText.color.g, 
+                _loadingText.color.b, Mathf.PingPong(Time.time, 1));
         }
 	}
+
+    IEnumerator LoadNewScene()
+    {
+        // This might be a bit much? seems fine for now.
+        yield return new WaitForSeconds(5);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(_scene, LoadSceneMode.Additive);
+        async.allowSceneActivation = false;
+        
+        while (async.progress < 0.9f)
+        {
+            yield return null;
+        }
+
+        async.allowSceneActivation = true;
+    }
 }

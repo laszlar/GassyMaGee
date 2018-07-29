f5051388260fb53ac1b874ec646ca27a654efe94
HOMEBREW-PC\Laszlo <laszlar@gmail.com>
2018-07-29 00:00:51 -0500

    Posts Re-implemented!

---
+++ Assets/Scripts/Scene1/PostManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostManager : MonoBehaviour
+{
+    //set the array for the posts
+    public GameObject[] postArray = new GameObject[4];
+
+    //Vector2 information
+    private Vector2 originalPOS;
+    private Vector2 nextPOS;
+
+    //Post GameObjects
+    private GameObject post;
+    private PostMover postScript;
+
+    private void Start()
+    {
+        //create the positions for spawning the posts
+        originalPOS = new Vector2(1.23f, -0.76f);
+        nextPOS = new Vector2((originalPOS.x + 3.293226313f), -0.76f);
+
+        //spawn the posts
+        for (int x = 0; x < 4; x++)
+        {
+            if (x == 0)
+            {
+                Instantiate(postArray[x], originalPOS, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(postArray[x], nextPOS, Quaternion.identity);
+                nextPOS.x += 3.293226313f;
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Scene1/PostMover.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostMover : MonoBehaviour
+{
+    //Mover's variables
+    private float speed = -1.0f;
+    private float slowSpeed = -0.5f;
+    private float fastSpeed = -1.5f;
+    private BoxCollider2D box;
+    public float boxWidth;
+    public float scale;
+    public float actualBoxWidth;
+
+    //Player's variables
+    private GameObject player;
+    PlayerMovement playerScript;
+
+    // Use this for initialization
+    void Start()
+    {
+        //find player object/script on startup
+        player = GameObject.Find("Player");
+        playerScript = player.GetComponent<PlayerMovement>();
+
+        //this object's width
+        box = GetComponent<BoxCollider2D>();
+        boxWidth = box.size.x;
+        scale = gameObject.transform.localScale.x;
+        actualBoxWidth = boxWidth * scale;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //If neither banana or parachute is active BG moves at regular speed.
+        if (!playerScript.parachuteEnabled && !playerScript.bananaEnabled ||
+            playerScript.parachuteEnabled && playerScript.bananaEnabled)
+            transform.Translate((speed * Time.deltaTime), 0f, 0f);
+
+        if (playerScript.bananaEnabled && !playerScript.parachuteEnabled)
+        {
+            SpeedUp();
+        }
+
+        if (playerScript.parachuteEnabled && !playerScript.bananaEnabled)
+        {
+            SlowDown();
+        }
+    }
+
+    void SlowDown()
+    {
+        transform.Translate((slowSpeed * Time.deltaTime), 0f, 0f);
+    }
+
+    void SpeedUp()
+    {
+        transform.Translate((fastSpeed * Time.deltaTime), 0f, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "FloorBoundaryDestroyer")
+        {
+            transform.Translate(actualBoxWidth * 4, 0f, 0f);
+        }
+    }
+}

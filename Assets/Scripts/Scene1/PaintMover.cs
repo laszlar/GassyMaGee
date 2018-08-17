b16fd24f04265d3d37e431884321c33395267de7
HOMEBREW-PC\Laszlo <laszlar@gmail.com>
2018-08-16 23:33:02 -0500

    Opps, powerups were taken out. I put them back in tho

--- Assets/Scripts/Scene1/PaintMover.cs
+++ Assets/Scripts/Scene1/PaintMover.cs
@@ -18,21 +18,11 @@ public class PaintMover : MonoBehaviour {
 
     public GameObject player;
     float paintPos;
-    float playerPos;
     float actualPos;
-    float offset = 5f;
-
-    // Use this for initialization
-    void Start ()
-    {
-        player = GameObject.Find("Player");
-	}
 
     // Update is called once per frame
     void FixedUpdate ()                          //Gives it a nice Sin wave for goofiness :)
     {
-        playerPos = player.transform.position.x;
-        actualPos = playerPos + offset;
         paintPos = transform.position.x;
 
         index += Time.deltaTime / paintSpeed; //this changes the X speed as it technically slows down time. Increase paintSpeed to slow it down further.
@@ -40,10 +30,8 @@ public class PaintMover : MonoBehaviour {
         float y = amplitudeY * Mathf.Sin(omegaY * index); // amplitude Y is the Y value as where it'll generate and omegaY is how many times it goes up and down.
         transform.localPosition = new Vector3((actualPos + x), y, 0);
 
-        if (paintPos <= 0.05f)
-        {
+        if (paintPos <= -0.5f)
             Destroy(gameObject);
-        }
     }
 
 
---
+++ Assets/Scripts/Scene1/PowerUps/PowerUpManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpManager : MonoBehaviour
+{
+    //GameObjects
+    public GameObject paintCan, parachute, banana;
+
+    //player script
+    PlayerMovement playerScript;
+
+    //Array
+    private GameObject[] powerUpHolder;
+
+    //Vector Positions
+    private Vector2 spawnPOS;
+
+    //float 
+    private float basicTimer;
+    private float paintTimer;
+
+    //bool
+    private bool spawnBasics;
+    private bool spawnPaint;
+
+    private void Start()
+    {
+        //Initiate the array
+        powerUpHolder = new GameObject[] { parachute, banana, paintCan };
+
+        //initialize the boolean
+        spawnBasics = false;
+        spawnPaint = false;
+
+        //initialize floats
+        basicTimer = 0f;
+        paintTimer = 0f;
+    }
+
+    private void Update()
+    {
+        //run those timers!
+        basicTimer += Time.deltaTime;
+        paintTimer += Time.deltaTime;
+
+        if (basicTimer >= 10.0f)
+            spawnBasics = true;
+        
+
+        if (paintTimer >= 20.0f)
+            spawnPaint = true;
+
+        if (spawnBasics)
+        {
+            Instantiate(powerUpHolder[Random.Range(0, 1)], new Vector2(5.0f, Random.Range(-0.4f, 1.2f)), Quaternion.identity);
+            spawnBasics = false;
+            basicTimer = 0f;
+        }
+
+        if (spawnPaint)
+        {
+            Instantiate(powerUpHolder[2], new Vector2(5.0f, Random.Range(-0.4f, 1.0f)), Quaternion.identity);
+            spawnPaint = false;
+            paintTimer = 0f;
+        }
+    }
+
+}

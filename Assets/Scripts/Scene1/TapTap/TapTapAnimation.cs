f2b492c3f559fa42dbc7a7fc8b1758ec1e08fd5c
HOMEBREW-PC\Laszlo <laszlar@gmail.com>
2018-08-17 00:10:22 -0500

    Partially added Tap Tap animation

---
+++ Assets/Scripts/Scene1/TapTap/TapTapAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapTapAnimation : MonoBehaviour
+{
+    //boolean 
+    public bool hasTapped;
+
+	// Use this for initialization
+	void Start () {
+        hasTapped = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!hasTapped && Input.GetMouseButton(0))
+            Destroy(gameObject);
+        else
+            return;
+	}
+}

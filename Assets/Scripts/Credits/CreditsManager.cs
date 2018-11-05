4551ffad6a221f96baf494ccc0f37513204e3e22
HOMEBREW-PC\Laszlo <laszlar@gmail.com>
2018-11-04 23:43:56 -0600

    Worked on Credits a bit

---
+++ Assets/Scripts/Credits/CreditsManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsManager : MonoBehaviour
+{
+    //Array
+    private string[] creditsList;
+
+    //GameObject
+    private Text gOText;
+
+
+	void Start ()
+    {
+        //initialize the string with our credits! 
+        creditsList = new string[] {"Designer/Programmer\n Laszlo K", "Art/Animation Director\n Ludmila Sosa",
+                                    "Senior Programmer\n Anthony Michelizzi", "Composer\n Cale Reneau"};
+
+        //find the text game object
+        gOText = GameObject.Find("Text").GetComponent<Text>();
+	}
+	
+
+	void Update ()
+    {
+        gOText.text = creditsList[3];
+	}
+}

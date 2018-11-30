8659cc0de04ca15dbb4fabd37090b7a0f0b8c270
HOMEBREW-PC\Laszlo <laszlar@gmail.com>
2018-11-29 23:56:04 -0600

    Almost done with the credits

--- Assets/Scripts/Credits/CreditsManager.cs
+++ Assets/Scripts/Credits/CreditsManager.cs
@@ -11,20 +11,69 @@ public class CreditsManager : MonoBehaviour
     //GameObject
     private Text gOText;
 
+    //bool
+    private bool hasLoaded;
+    private bool startStopTimer = true;
+    private bool changeCreditsFlag;
+
+    //int
+    private int orderOfCredits;
+
+    //float
+    private float timer;
 
 	void Start ()
     {
         //initialize the string with our credits! 
-        creditsList = new string[] {"Designer/Programmer\n Laszlo K", "Art/Animation Director\n Ludmila Sosa",
+        creditsList = new string[] {"Designer/Programmer\n Laszlo Kecskes", "Art/Animation Director\n Ludmila Sosa",
                                     "Senior Programmer\n Anthony Michelizzi", "Composer\n Cale Reneau"};
 
         //find the text game object
         gOText = GameObject.Find("Text").GetComponent<Text>();
-	}
-	
 
-	void Update ()
-    {
-        gOText.text = creditsList[3];
+        //set text load to false on start
+        hasLoaded = false;
+
+        orderOfCredits = 0;
 	}
+
+
+    void Update()
+    {
+        ////////////////////////////////////
+        //need to finalize the credits list!
+        ////////////////////////////////////
+        if (!hasLoaded)
+        {
+
+            if (startStopTimer)
+            timer += Time.deltaTime;
+
+            //display the credit for about 1.8 seconds and have a blank screen for 0.2 seconds
+            if (timer > 0.2f && timer < 2.0f)
+            {
+                gOText.text = creditsList[orderOfCredits];
+            }
+
+            //turn off the credits after it reaches its final credit
+            if (timer >= 2.0f && orderOfCredits == 3)
+            {
+                gOText.text = "";
+                timer = 0f;
+                startStopTimer = false;
+            }
+
+            //change to the next credit after 2 seconds
+            if (timer >= 2.0f)
+            {
+                gOText.text = "";
+
+                if (orderOfCredits < 3)
+                {
+                    orderOfCredits++;
+                }
+                timer = 0f;
+            }
+        }
+    }
 }
